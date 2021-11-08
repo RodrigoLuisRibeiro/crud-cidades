9cd3cc1369bf2d6f332000930a41765ecc8b8e84
Rodrigo Luís Ribeiro <rodrigo.ribeiro0909@gmail.com>
2021-11-08 14:49:51 -0300

    projeto-server-estatica

--- crud-cidades/src/main/java/com/edu/utfpr/cp/espjava/crudcidades/CrudCidadesApplication.java
+++ crud-cidades/src/main/java/com/edu/utfpr/cp/espjava/crudcidades/CrudCidadesApplication.java
@@ -3,9 +3,11 @@ package com.edu.utfpr.cp.espjava.crudcidades;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CrudCidadesApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CrudCidadesApplication.class, args);
 	}
