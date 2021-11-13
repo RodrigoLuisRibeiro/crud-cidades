1bb08f70db61592aafca147e01d1bbeb960c9f16
Rodrigo Luís Ribeiro <rodrigo.ribeiro0909@gmail.com>
2021-11-13 18:44:51 -0300

    autorizacao

--- crud-cidades/src/main/java/com/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
+++ crud-cidades/src/main/java/com/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.edu.utfpr.cp.espjava.crudcidades;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,6 +23,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.withUser("Rodrigo")
 		.password(cifrador().encode("teste123"))
 		.authorities("admin");
+	}
+	
+	
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+			.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/").hasAnyAuthority("listar", "admin")
+			.antMatchers("/criar").hasAnyAuthority("admin")
+			.antMatchers("/excluir").hasAnyAuthority("admin")
+			.antMatchers("/preparaAlterar").hasAnyAuthority("admin")
+			.antMatchers("/alterar").hasAnyAuthority("admin")
+			.anyRequest().denyAll()
+			.and()
+			.formLogin().permitAll();
 	}
 	
 	@Bean
